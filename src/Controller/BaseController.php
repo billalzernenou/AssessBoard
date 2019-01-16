7c8935bb04440193f9ee709e3bc1cee13dc49e3c
Tend0 <aurelien.pgt@hotmail.fr>
2019-01-16 15:26:53 +0100

    Apt param admin (#5)
    * page sign in
    
    * init create page
    
    * archi & navbar/about created
    
    * merge sign-in to dev
    
    * Add back/admin

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -16,4 +16,24 @@ class BaseController extends AbstractController
             'controller_name' => 'BaseController',
         ]);
     }
+
+   /**
+     * @Route("/sign-in", name="sign-in")
+     */
+    public function signIn()
+    {
+        return $this->render('front/sign-in.html.twig', ['
+        controller_name' => 'BaseController',
+        ]);
+    }
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return $this->render('front/about/about.html.twig', [
+
+            'controller_name' => 'BaseController',
+        ]);
+    }
 }
